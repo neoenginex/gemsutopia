9cf9fc92d718b183e94ab97f18ac05c9e9a406a5
neoenginex <neoenginex@gmail.com>
2025-08-08 16:03:09 -0600

    Update(6)

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.hcaptcha.com https://hcaptcha.com; frame-src https://hcaptcha.com https://newassets.hcaptcha.com; style-src 'self' 'unsafe-inline' https://hcaptcha.com https://newassets.hcaptcha.com; connect-src 'self' https://hcaptcha.com https://api.hcaptcha.com https://*.supabase.co;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
